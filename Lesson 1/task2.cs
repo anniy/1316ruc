026350db66a8512534f4962e4630b0d4639b9cd0
anniy <akaneva@uni-ruse.bg>
2013-10-14 22:29:53 +0300

    Lesson 1
    Tasks

---
+++ Lesson 1/task2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 0,
+                number2 = 0,
+                number3 = 0;
+
+            try
+            {
+                Console.Write("Enter first number: ");
+                number1 = int.Parse(Console.ReadLine());
+                Console.Write("Enter second number: ");
+                number2 = int.Parse(Console.ReadLine());
+                Console.Write("Enter third number: ");
+                number3 = int.Parse(Console.ReadLine());
+                Console.WriteLine("The sum is: {0}", number1 + number2 + number3);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The input value is not a number;");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson 1/task3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte numSByte = -115; //  -44, 97, -115, 112
+            byte numByte = 224;
+            short numShort = 20000; // -10000, 20000, 1990
+            ushort numUShort = 52130;
+            int numInt = 970700000; // -1000000, 4825932, 970700000
+            ulong numULong = 123456789123456789;
+
+            Console.WriteLine("sbyte -> {0}", numSByte);
+            Console.WriteLine("byte -> {0}", numByte);
+            Console.WriteLine("short -> {0}", numShort);
+            Console.WriteLine("ushort -> {0}", numUShort);
+            Console.WriteLine("int -> {0}", numInt);
+            Console.WriteLine("ulong -> {0}", numULong);
+            Console.ReadLine();
+        }
+    }
+}
