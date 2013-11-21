06f645b6ff5287522034f3548e3f1520f04eaabb
anniy <akaneva@uni-ruse.bg>
2013-11-21 21:30:05 +0200

    Lesson 5
    Lesson 5

---
+++ Lesson 5/Task1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace task1
+{
+    class Task1
+    {
+        static string Numbers(int n)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < n; i++)
+            {
+                sb.Append(i);
+                sb.Append(',');
+            }
+            return sb.ToString();
+       }
+
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            string result = "";
+            int n;
+            bool input = int.TryParse(Console.ReadLine(), out n);
+
+            if ((!input) || (n > 100000) || (n < 10000))
+            {
+                Console.WriteLine("ERROR");
+                return;
+            }
+
+            watch.Start();
+
+            result = Numbers(n);
+
+            watch.Stop();
+
+            Console.WriteLine("{0}\n {1}", n, result);
+            Console.WriteLine("{0} sec.", (float) watch.ElapsedMilliseconds / 1000);
+        }
+    }
+}
