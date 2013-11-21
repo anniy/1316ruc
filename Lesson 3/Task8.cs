ed7f31cfe123ae8f8170fb5e10477984d701b646
anniy <akaneva@uni-ruse.bg>
2013-11-21 21:29:43 +0200

    Lesson 3
    Lesson 3

--- Lesson 3/Task8.cs
+++ Lesson 3/Task8.cs
@@ -7,9 +7,50 @@ namespace task8
 {
     class Task8
     {
+        static int Euclid(int n1, int n2)
+        {
+            if (n1 == n2)
+            {
+                return n1;
+            }
+            else
+            {
+                if (n1 > n2)
+                {
+                    return Euclid(n1 - n2, n2);
+                }
+                else
+                {
+                    return Euclid(n1, n2 - n1);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            int num1;
+            int num2;
+            int nod;
+            int nok;
 
+            string[] token;
+
+            token = Console.ReadLine().Split();
+
+            bool input1 = int.TryParse(token[0], out num1);
+            bool input2 = int.TryParse(token[1], out num2);
+
+            if (!input1 || !input2)
+            {
+                Console.WriteLine("ERROR");
+                return;
+            }
+
+            nok = Euclid(num1, num2);
+            nod = (num1 * num2) / nok;
+
+            Console.WriteLine("NOK({0},{1}) = {2}", num1, num2, nok);
+            Console.WriteLine("NOD({0},{1}) = {2}\n", num1, num2, nod);
         }
     }
 }
